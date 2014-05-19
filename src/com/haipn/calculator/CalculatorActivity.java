69b36b2cab36e7867456c2ca7385daa1a8e0b0bd
HaiPN <phamngochai.a7@gmail.com>
2014-05-19 17:41:42 +0700

    add butoon add

--- src/com/haipn/calculator/CalculatorActivity.java
+++ src/com/haipn/calculator/CalculatorActivity.java
@@ -1,5 +1,31 @@
 package com.haipn.calculator;
 
-public class CalculatorActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
+public class CalculatorActivity extends Activity {
+	private Button mBtnAdd;
+	private LinearLayout mLlReceived;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.calculator);
+		
+		mBtnAdd = (Button) findViewById(R.id.btnAdd);
+		mLlReceived = (LinearLayout) findViewById(R.id.llReceived);
+		mBtnAdd.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EditText newAmount = new EditText(CalculatorActivity.this);
+				mLlReceived.addView(newAmount);
+			}
+		});
+	}
 }
